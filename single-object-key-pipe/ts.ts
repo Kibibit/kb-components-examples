053beec2ec38a46b541cb064edb9ff28bae3b616
Neil Kalman <Neilkalman@gmail.com>
2019-02-20 15:26:50 +0200

    Update ts.ts

--- single-object-key-pipe/ts.ts
+++ single-object-key-pipe/ts.ts
@@ -1,1 +1,20 @@
-formFieldErrors = { required: true, pattern: true };
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'kb-docs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+}
