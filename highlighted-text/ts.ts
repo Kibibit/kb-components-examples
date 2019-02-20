8f5b100473340ce3de57ad49f1ad20991c1a3bfc
Neil Kalman <Neilkalman@gmail.com>
2019-02-20 14:35:39 +0200

    Create ts.ts

---
+++ highlighted-text/ts.ts
@@ -0,0 +1,7 @@
+export class AppComponent {
+  content = [
+    'Hello there! This kb-highlighted-text should highlight all',
+    'the sections matching the searchTerm (initially: hello). It is',
+    'case insensitive so it should find all the instances regardless'
+  ].join(' ');
+}
